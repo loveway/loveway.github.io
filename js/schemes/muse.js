fe1dcc06c17acfaec02cdeaa95baab29fc80fba3
HenryCheng <1004502125@qq.com>
2019-07-07 12:38:01 +0800

    Site updated: 2019-07-07 12:38:00

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jul 06 2019 17:24:35 GMT+0800 (GMT+08:00)
+// build time:Sun Jul 07 2019 12:33:38 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
