f76b0a4622f8ca73a359aa46ae31cfd813f93196
HenryCheng <1004502125@qq.com>
2019-07-19 17:09:04 +0800

    Site updated: 2019-07-19 17:09:04

--- js/clipboard-use.js
+++ js/clipboard-use.js
@@ -1,3 +1,1 @@
-// build time:Fri Jul 19 2019 17:06:17 GMT+0800 (GMT+08:00)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
-//rebuild by neat 
+window,document,$(".highlight .code pre").before('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-clipboard"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});
